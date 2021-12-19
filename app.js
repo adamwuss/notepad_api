ef1a36807849eafa465e552b0c84fc2b75f2db11
Adam Wuss <adamwusspl@gmail.com>
2021-12-19 01:36:25 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { port } = require('./config');
+const apiRouter = require('./routes/api');
+
+const app = express();
+
+// routes
+app.use('/', apiRouter);
+
+// server
+app.listen(port, () => {
+  console.log('Server listening');
+});
